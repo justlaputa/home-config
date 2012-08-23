27991ab0efadd705a5da01efc50ad40f5a564f3f
justlaputa <justlaputa@gmail.com>
2012-08-24 00:22:57 +0800

    add org-agenda settings

--- .emacs.d/my-config/gtd-settings.el
+++ .emacs.d/my-config/gtd-settings.el
@@ -6,7 +6,18 @@
 ;; org-mode
 
 (setq org-todo-keywords '("TODO" "STARTED" "WAITING" "DONE"))
+
 (setq org-enforce-todo-dependencies 1)
+
+(setq org-startup-folded 'content)
+
+(setq org-hide-leading-stars 'hidestars)
+
+;; org-agenda settings
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+(setq org-agenda-files '("~/Dropbox/Docs/orgs/agenda.org"
+                         "~/Dropbox/Docs/orgs/work.org"))
 
 ;;========================================
 ;; remeber
