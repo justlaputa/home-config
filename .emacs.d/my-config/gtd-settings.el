3d5944ef3c7479a62956889a95e24af3c7822955
justlaputa <justlaputa@gmail.com>
2012-09-02 21:11:56 +0800

    remove 'remember' emace package

--- .emacs.d/my-config/gtd-settings.el
+++ .emacs.d/my-config/gtd-settings.el
@@ -17,12 +17,32 @@
 ;; org-agenda settings
 (global-set-key (kbd "C-c a") 'org-agenda)
 
-(setq org-agenda-files '("~/Dropbox/Docs/orgs/agenda.org"
+(setq org-agenda-files '("~/Dropbox/Docs/orgs/tasks.org"
                          "~/Dropbox/Docs/orgs/work.org"
                          "~/Dropbox/Docs/orgs/notes.org"))
 
 ;; org-capture settings
 (setq org-default-notes-file (concat org-directory "notes.org"))
 (global-set-key (kbd "C-c c") 'org-capture)
+
+(setq org-capture-templates
+      '(("n" "Notes" entry (file+datetree (concat org-directory "notes.org"))
+         "* %^{Description} %^g %?
+  Added: %U")
+        ("t" "Todo items")
+        ("tl" "Life" entry (file+headline (concat org-directory "tasks.org") "Life")
+         "* TODO %^{What I want to do?}\n  Added: %U")
+        ("th" "Hack" entry (file+headline (concat org-directory "tasks.org") "Hack")
+         "* TODO %^{What I want to hack?}\n  Added: %U")
+        ("w" "Work tasks")
+        ("wm" "Meetings" entry (file+headline (concat org-directory "work.org") "Meeting")
+         "* TODO %^{Meeting again?}\n  %^U\n  Added: %U")
+        ("wd" "Development" entry (file+headline (concat org-directory "work.org") "Development")
+         "* TODO %^{What ticket?}\n   Added: %U")
+        ("s" "Shop item" checkitem (file+headline (concat org-directory "tasks.org") "Shop")
+         "- [ ] %^{Item I want buy}\n  Added: %U")
+        ("l" "TimeLogger" entry (file+datetree (concat org-directory "timelog.org"))
+         "** %U - %^{Activity} :TIME:")
+        ))
 
 (provide 'gtd-settings)
